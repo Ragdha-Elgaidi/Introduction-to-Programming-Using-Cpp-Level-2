6d42fab448590e925a1033c8ee7f9188d733126b
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-02-11 11:07:53 +0200

    Create Float Format (printf) .cpp

---
+++ Float Format (printf) .cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+int main() 
+{
+	float PI = 3.14159265;
+
+	//Precision specification   
+	printf("Precision specification of %.*f\n", 1, PI);
+	printf("Precision specification of %.*f\n", 2, PI); 
+	printf("Precision specification of %.*f\n", 3, PI); 
+	printf("Precision specification of %.*f\n", 4, PI); 
+
+	float x = 7.0, y = 9.0;
+	
+	printf("\nThe float division is : %.3f / %.3f = %.3f \n\n", x, y, x / y); 
+	
+	double d = 12.45;
+	
+	printf("The double value is : %.3f \n", d);
+	printf("The double value is : %.4f \n", d);
+	
+	return 0; 
+}
+/*
+Precision specification of 3.1
+Precision specification of 3.14
+Precision specification of 3.142
+Precision specification of 3.1416
+
+The float division is : 7.000 / 9.000 = 0.778
+
+The double value is : 12.450
+The double value is : 12.4500
+*/
