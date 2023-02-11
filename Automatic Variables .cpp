fcbc6ceae4698fbafc945c4eef38d93085d33d12
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-02-11 10:44:19 +0200

    Create Automatic Variables .cpp

---
+++ Automatic Variables .cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	auto x = 10; //Type Integer
+	auto y = 12.5; //Type Double
+	auto z = "Ragdha Elgaidi"; //Type String   
+	
+	cout << x << endl;   
+  cout << y << endl; 
+  cout << z << endl;
+ 
+  return 0;
+}
