cc45df827be08156ee77aaeddbe6836163c5a8dc
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-02-12 07:06:04 +0200

    Create  Vector of Structure .cpp

---
+++ Vectors/Vector of Structure .cpp
@@ -0,0 +1,47 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+
+struct stEmployee
+{
+	string FirstName; 
+	string LastName; 
+	float Salary;
+}; 
+
+int main() 
+{ 
+	vector <stEmployee> vEmployees; 
+	stEmployee tempEmployee;    
+
+	tempEmployee.FirstName = "Mohammed";
+	tempEmployee.LastName = "Abu-Hahdoud"; 
+	tempEmployee.Salary = 5000;   
+
+	vEmployees.push_back(tempEmployee);  
+
+	tempEmployee.FirstName = "Ali";  
+	tempEmployee.LastName = "Maher"; 
+	tempEmployee.Salary = 300;   
+
+	vEmployees.push_back(tempEmployee); 
+
+	tempEmployee.FirstName = "Aya";   
+	tempEmployee.LastName = "Omran";  
+	tempEmployee.Salary = 1000;    
+
+	vEmployees.push_back(tempEmployee);
+
+	cout << "Employees Vector: \n\n";
+
+	for (stEmployee &Employee : vEmployees)
+	{      
+		cout << "FirstName: " << Employee.FirstName << endl;
+		cout << "LastName : " << Employee.LastName << endl;  
+		cout << "Salary   : " << Employee.Salary << endl;   
+        cout << endl;     
+	} 
+	cout << endl;
+
+	return 0; 
+}
